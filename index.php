f7ce193949aedcad792c12e2abd7d38078d89633
Anakareli Gonzalez <anakgonzalez@csumb.edu>
2018-02-12 07:26:10 +0000

    slotmachine

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+    include 'inc/functions.php';
+    ?>
+<!DOCTYPE html>
+<html> 
+    <head>
+        <title>777 Slot Machine</title>
+        <style>
+            @import url("css/styles.css");
+        </style>
+    </head>
+    
+    <body>
+        <?php
+            play();
+        ?>
+        <div id = "main">
+            <form>
+                <input type="submit" value = "Spin!"/>
+            </form>
+        </div>
+    </body>
+</html>
